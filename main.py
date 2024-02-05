ac37b122b6e2b62a817b789e23e32a3c20a8ec6c
Kate-Fitzgerald <kate.fitzgerald@students.fhu.edu>
2024-02-05 13:29:52 -0600

    update main.py
    updated changing the string to have no whitespace in between words

--- main.py
+++ main.py
@@ -2,10 +2,15 @@
 def palindrome(word):
     word = word.strip()
     word = word.upper()
+    word = word.split()
+    for i in word:
+        newword = str(word).strip
+    newword = "".join(word)
+
     
-    if word.isalpha() == True:
-        reverse_word = word[::-1]
-        if reverse_word == word:
+    if newword.isalpha() == True:
+        reverse_word = newword[::-1]
+        if reverse_word == newword:
             return True
         else:
             return False
